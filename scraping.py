36b018d1aa1c9788ea419d66a8eeec62b3dc7f20
neng9090 <nenengrohmatiah99@gmail.com>
2024-09-18 21:26:53 +0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
